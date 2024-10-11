458fa27f5a98ff41779fb51da97e54c939adb4d8
CgyDesign <cgy_design@126.com>
2024-10-11 12:12:00 +0800

    first update

---
+++ src/Attoorm.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * atto-orm framework for Attobox PHP framework
+ * 
+ * 此文件夹应添加到 vendor/attokit/attobox/modules 路径下
+ * 此文件必须放置在 vendor/attokit/attobox/modules 路径下
+ */
+
+namespace Atto\Box;
+
+class Attoorm
+{
+    
+}
